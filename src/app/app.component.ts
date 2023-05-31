0cc73d134b2dc89c749d5b6ad77434af8b0dab0f
tambatra <tambatra00000000@gmail.com>
2023-05-31 20:36:47 +0200

    Adding button handler function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'angular-oauth';
+  onSignInClicked() {
+    console.log('Should sign in');
+  }
 }
